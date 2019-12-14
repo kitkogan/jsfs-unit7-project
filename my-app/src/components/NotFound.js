5d6c4d885a98baee7c8c152b82c1e10c56ab4ce9
Kit Kogan <kitkogan@users.noreply.github.com>
2019-12-13 21:03:36 -0600

    Update NotFound.js

--- my-app/src/components/NotFound.js
+++ my-app/src/components/NotFound.js
@@ -1,32 +1,4 @@
-import React, { Component } from 'react';
-
-export default class Search extends Compondent {
-
-    state = {
-        searchText: ''
-    }
-
-    onSearchChange = (e) => {
-        this.setState({ searchText: e.target.value });
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.onSearch(this.state.searchText);
-        this.setState({ searchText: '' });
-    }
-
-    render() {
-        return (
-            <div>
-                <form className="search-form" onSubmit={this.handleSubmit}>
-                    <input type="search" name="search" value={this.state.searchText} placeholder="Search" onChange={this.onStateChange} required/>
-                    <button type="submit" className="search-button">Search</button>
-                </form>
-            </div>
-        )
-    }
-}
+import React from 'react';
 const NotFound = () => (
     <div>
         <h3>Sorry, the page you requested cannot be found</h3>
