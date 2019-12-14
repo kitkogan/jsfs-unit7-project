3be214a9b10a5397387da6c1bd1f8e09f6ebf15a
Kit Kogan <kitkogan@users.noreply.github.com>
2019-12-13 21:05:00 -0600

    Create Search.js

---
+++ my-app/src/components/Search.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class Search extends Compondent {
+
+    state = {
+        searchText: ''
+    }
+
+    onSearchChange = (e) => {
+        this.setState({ searchText: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSearch(this.state.searchText);
+        this.setState({ searchText: '' });
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="search-form" onSubmit={this.handleSubmit}>
+                    <input type="search" name="search" value={this.state.searchText} placeholder="Search" onChange={this.onStateChange} required/>
+                    <button type="submit" className="search-button">Search</button>
+                </form>
+            </div>
+        );
+    }
+}
