6de839fed3989570e5a05e442a036680b63784aa
Kit Kogan <kitkogan@users.noreply.github.com>
2019-12-13 19:34:10 -0600

    add Photo compotent

---
+++ my-app/src/components/Photo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Photo = props => (
+    <li className="photo">
+    <a href={`https://www.flickr.com/photo.gne?id=${props.pathId}`} target="_blank" rel="noopener noreferrer">
+       <img src={props.url} alt=""/>
+
+       </a>
+    </li>
+);
+
+export default Photo;
